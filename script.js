516459a4542327010f50d63c630ace7526a6a5e5
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2024-12-18 14:56:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,34 @@
-
-/* Multiplication Quiz 
+/* MULTIPLICATION QUIZ
  * This program should ask five multiplication questions, displaying which question
  * is being asked, and counting the number of equations with at least one error.
  * FLOWCHART: https://lucid.app/lucidchart/5a3164fd-459f-494d-9cae-b4a6be593b13/view
  */
+
 main()
+
+/* main controls the program. Calling askFive() it provides feedback depending on the 
+ * number wrong returned: 0 = "Perfect!" otherwise it says how many wrong. 
+ * @param none
+ * @return none
+ */
 function main() {
-    let score = askFive();
-    if (score == 0) alert("Perfect!");
-    else alert("You had "+score+" errors");
+
 }
 
-/* askFive calls askQuestion five times, counting and returning number wrong
+/* askFive calls askQuestion() five times, sending the question number as an argument. 
+ * It counts number wrong returned, and return number wrong to main().
  * @param: none
  * @return: score (0-5)
  */
 function askFive() {
-    let wrong = 0;
-    for (let question = 1; question <= 5; question++) {
-        wrong += askQuestion(question);
-    }
-    return wrong;
+
 }
 
-/* askQuestion asks a multiplication question, returns 1 if incorrect
+/* askQuestion asks a multiplication question, using the quesiton parameter to say which
+ * question is being asked.  It returns 1 if incorrect, 0 if correct.
  * @param: question (integer 1-5)
  * @return: integer (0 or 1)
  */
 function askQuestion(question){
-    let a = Math.floor(Math.random()*7)+3;
-    let b = Math.floor(Math.random()*7)+3;
-    let product = a*b;
-    let equation = a + " * " + b + " = ?";
-    let answer = prompt(equation);
-    if (answer == "q"){
-        alert("Quitter!");
-        return 2;
-    }
-    else if (answer == product){
-       alert("Correct!");
-       return 0;
-    } 
-    else {
-        alert("incorrect");
-        return 1;   
-    } 
+  
 }
-
-/* STAGE 3: MULTIPLE TRIES (CHALLENGE!) */
-
-/* Refactor askQuestion per whileQuestions askFive calls askQuestion five times, counting and returning number wrong
- * Use the whileQuestions tab in the flowchart to guide you. 
- * @param: none
- * @return: score (0-5)
- */
